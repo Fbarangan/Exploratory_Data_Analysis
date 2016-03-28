a8609a7b80b0b3b031020c76f0e8b822f32ff674
Fbarangan <franb86.2@gmail.com>
2016-03-28 02:00:46 -0400

    2nd commit

--- Plot_R_Code/Plot1.R
+++ Plot_R_Code/Plot1.R
@@ -1,4 +1,7 @@
 #File Management and data download:
+library(dplyr)
+
+
 if (!file.exists("RawData"))
    {dir.create("RawData")}
 
@@ -24,3 +27,29 @@ colnames(Data)[6] = "Global_intensity"
 colnames(Data)[7] = "Sub_metering_1"
 colnames(Data)[8] = "Sub_metering_2"
 colnames(Data)[9] = "Sub_metering_3"
+
+dim(Data)
+
+#remove row 1
+rawData2 <- tbl_df(Data)
+rawData2 <-  rawData2[c(2:2075260),]
+rawData3 <- mutate(rawData2, Date = as.POSIXct(rawData2$Date, format = "%d/%m/%Y"))
+rawData4 <- select(rawData3, -Time)
+rawData5 <- filter(rawData4, Date == "2007-02-01" | Date == "2007-02-02")
+
+# convert  Global active power to numeric
+
+GlobalActivePower <- as.numeric(rawData5$Global_active_power)
+rawData5 <- cbind(rawData5,GlobalActivePower)
+
+
+#Plot Data
+par(mfrow = c(1,1))
+hist(rawData5$GlobalActivePower, main = "Global Active Power",
+     xlab = "Global Active Power (Kilowatts)",
+     ylab = "Frequency",
+     col = "red")
+
+#Copy to PNG
+dev.copy(png, file = "plot1.png" )
+dev.off()
