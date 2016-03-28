71ee84418efe9f0fc1b6ddcfc73453a50449d640
Fbarangan <franb86.2@gmail.com>
2016-03-28 02:04:10 -0400

    3rd commit

--- Plot_R_Code/Plot1.R
+++ Plot_R_Code/Plot1.R
@@ -1,7 +1,7 @@
 #File Management and data download:
 library(dplyr)
 
-
+#Create file
 if (!file.exists("RawData"))
    {dir.create("RawData")}
 
@@ -18,6 +18,7 @@ rawData <- file()
 Data <- read.table (file = "./RawData/household_power_consumption.txt", sep = ";", na.strings = "?", stringsAsFactors = FALSE, header = FALSE, skip = 0)
 
 #Rename column
+
 colnames(Data)[1] = "Date"
 colnames(Data)[2] =  "Time"
 colnames(Data)[3] = "Global_active_power"
@@ -30,14 +31,15 @@ colnames(Data)[9] = "Sub_metering_3"
 
 dim(Data)
 
-#remove row 1
+#remove row 1. Label is redundant, convert to tbl format, convert date to right
+#format, then filter the desired dates
 rawData2 <- tbl_df(Data)
 rawData2 <-  rawData2[c(2:2075260),]
 rawData3 <- mutate(rawData2, Date = as.POSIXct(rawData2$Date, format = "%d/%m/%Y"))
 rawData4 <- select(rawData3, -Time)
 rawData5 <- filter(rawData4, Date == "2007-02-01" | Date == "2007-02-02")
 
-# convert  Global active power to numeric
+# convert  Global active power to numeric, then bind column back to the main data
 
 GlobalActivePower <- as.numeric(rawData5$Global_active_power)
 rawData5 <- cbind(rawData5,GlobalActivePower)
