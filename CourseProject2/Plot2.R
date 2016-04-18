4e6d1375eb990099677eb571695edcd5f51a6405
Felix Barangan <franb86.2@gmail.com>
2016-04-18 01:30:30 -0400

    Create Plot2.R

---
+++ CourseProject2/Plot2.R
@@ -0,0 +1,48 @@
+#File Management and data Download
+library(dplyr)
+library(ggplot2)
+
+#get Directorory
+getwd()
+
+# file location -  "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+# Create the file
+if (!file.exists("PM25_Raw_Data")) {
+  dir.create("PM25_Raw_Data")
+}
+
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+
+download.file(fileUrl, destfile = "./PM25_Raw_Data/PeerAssessment.zip")
+dateOfDownload <- date()
+
+# Click the PeerAssessment.zip to unzip.
+# 2 File are extracted:
+#    Source_Classification_Code.rds
+#    summarySCC_PM25.rds
+
+# Make sure to go to the right directory
+
+# Read  .rds file
+NEI <- readRDS ("./CourseProject2/PM25_Raw_Data/PeerAssessment/summarySCC_PM25.rds")
+SCC <- readRDS ("./CourseProject2/PM25_Raw_Data/PeerAssessment/Source_Classification_Code.rds")
+
+# Turn table into dplyr
+NEI_ddply <- tbl_df(NEI)
+SCC_ddply <- tbl_df(SCC)
+
+# Plot 2
+#Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") from 1999 to 2008? Use the base plotting system to make a plot answering this question.
+
+Baltimore_Emission <- NEI_ddply %>%
+        filter(fips == "24510") %>%
+        group_by(year) %>%
+        summarise(Total = sum(Emissions )) %>%
+        select(Year = year, Emission = Total)
+
+plot(Baltimore_Emission$Year, Baltimore_Emission$Emission,
+                                xlab = "Year",
+                                ylab = "Total Emission",
+                                main = "Total Emission (PM2.5) for Baltimore City",
+                                pch= 1)
