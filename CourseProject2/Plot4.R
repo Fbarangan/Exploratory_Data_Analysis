ac477451ff3167acf1a5ca0371d0a963bc27c817
Felix Barangan <franb86.2@gmail.com>
2016-04-18 01:32:27 -0400

    Create Plot4.R

---
+++ CourseProject2/Plot4.R
@@ -0,0 +1,53 @@
+#File Management and data Download
+library(dplyr)
+library(ggplot2)
+
+#get Directorory
+getwd()
+
+# file location -  "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+# Create the file
+if (!file.exists("PM25_Raw_Data")) {
+  dir.create("PM25_Raw_Data")
+}
+
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+
+download.file(fileUrl, destfile = "./PM25_Raw_Data/PeerAssessment.zip")
+dateOfDownload <- date()
+
+# Click the PeerAssessment.zip to unzip.
+# 2 File are extracted:
+#    Source_Classification_Code.rds
+#    summarySCC_PM25.rds
+
+# Make sure to go to the right directory
+
+# Read  .rds file
+NEI <- readRDS ("./CourseProject2/PM25_Raw_Data/PeerAssessment/summarySCC_PM25.rds")
+SCC <- readRDS ("./CourseProject2/PM25_Raw_Data/PeerAssessment/Source_Classification_Code.rds")
+
+# Turn table into dplyr
+NEI_ddply <- tbl_df(NEI)
+SCC_ddply <- tbl_df(SCC)
+
+
+# Plot 4
+# Across the United States, how have emissions from coal combustion-related sources changed from 1999–2008?
+
+coal <- SCC_ddply %>%
+        filter(grepl('Coal', Short.Name) | grepl('Coal', EI.Sector))
+
+
+coal_NEI <- NEI_ddply %>%
+        filter(SCC %in% coal$SCC) %>%
+        group_by(year) %>%
+        summarise(Total = sum(Emissions)) %>%
+        select(Year = year, Emission = Total)
+
+plot_coal_NEI <- qplot(Year, Emission, data = coal_NEI,
+                       geom = "line") +
+        ggtitle("Total Emission of PM2.5 in Baltimore City with Coal Combustion Realted Source") +
+        xlab ("Years(s)") +
+        ylab ("Total Emission (PM2.5)")
